2e0c154f6039ce491a03d266d16643290ae23b36
Vasili <avazingg@gmail.com>
2023-12-25 14:10:41 +0100

    feat: vigenere cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    //ss
+
+  constructor(direct = true) {
+      this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (arguments.length < 2 || !message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+      let result = '';
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+      for (let i = 0; i < message.length; i++) {
+          if (key.length < message.length) {
+              key += key[i];
+          }
+      }
+      let j = 0;
+      for (let i = 0; i < message.length; i++) {
+          let index = (symbols.length + symbols.indexOf(message[i]) + symbols.indexOf(key[j])) % symbols.length;
+          if (symbols.includes(message[i])) {
+              result += symbols[index];
+              j++;
+          } else {
+              result += message[i];
+          }
+      }
+      if (this.direct){
+        return result;
+      }
+      return result.split('').reverse().join('');
+      
+  }
+
+  decrypt(result, key) {
+    if (arguments.length < 2 || !result || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let message = '';
+    result = result.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0; i < result.length; i++) {
+        if (key.length < result.length) {
+            key += key[i];
+        }
+    }
+    let j = 0;
+      for (let i = 0; i < result.length; i++) {
+          let index = (symbols.length + symbols.indexOf(result[i]) - symbols.indexOf(key[j])) % symbols.length;
+          if (symbols.includes(result[i])) {
+              message += symbols[index];
+              j++;
+          } else {
+              message += result[i];
+          }
+      }
+      if (this.direct){
+        return message;
+      }
+      return message.split('').reverse().join('');
   }
 }
 
